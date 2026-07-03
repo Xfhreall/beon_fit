6ca53e5170c0fe7308f3986a4a9bc841325906a1
Xfhreall <farelrizqi377@gmail.com>
2026-07-03 18:36:34 +0700

    feat(ui): add radix nova component set

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,9 +1,10 @@
 import { Head, Link, usePage } from '@inertiajs/react';
 import { dashboard, login } from '@/routes';
 import { register } from '@/routes';
+import type { SharedPageProps } from '@/types';
 
 export default function Welcome() {
-    const { auth } = usePage().props;
+    const { auth } = usePage<SharedPageProps>().props;
 
     return (
         <>
--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,3 +1,11 @@
+import type { Auth } from './auth';
+
 export type * from './auth';
 export type * from './navigation';
 export type * from './ui';
+
+export type SharedPageProps = {
+    name: string;
+    auth: Auth;
+    sidebarOpen: boolean;
+};
